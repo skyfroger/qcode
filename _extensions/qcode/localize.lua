1347221ffe13a11d39270f442ce06d37c71ca71b
skyfroger <skyfroger@gmail.com>
2026-06-21 13:39:59 +0300

    feat: localization

---
+++ _extensions/qcode/localize.lua
@@ -0,0 +1,65 @@
+
+-- // LOCALISATIONS STRINGS //
+local loc_data = {
+  en = {
+    showOptionalParams = "Show optional parameters",
+    showTypes = "Show types",
+    showDiagram = "Show diagram",
+    parameters = "Parameters",
+    returnValue = "Return value",
+    examples = "Examples",
+    codeExample = "Code",
+    codeOutput = "Output",
+    linkToDocs = "Link to the documentation",
+    runCode = "Run script",
+    stopCode = "Stop script",
+    hideExecResults = "Hide result",
+    restoreSourceCode = "Restore code",
+    copyCode = "Copy",
+    saveCode = "Save to file...",
+    listOfFunctions = "List of used functions",
+    searchInputPlaceholder = "Type function or method name",
+  },
+  ru = {
+    showOptionalParams = "Показать необязательные параметры",
+    showTypes = "Показать типы",
+    showDiagram = "Показать диаграмму",
+    parameters = "Параметры",
+    returnValue = "Возвращаемое значение",
+    examples = "Примеры",
+    codeExample = "Код",
+    codeOutput = "Вывод",
+    linkToDocs = "Ссылка на документацию (англ.)",
+    runCode = "Запустить",
+    stopCode = "Остановить скрипт",
+    hideExecResults = "Скрыть результат работы скрипта",
+    restoreSourceCode = "Восстановить исходный код",
+    copyCode = "Скопировать код",
+    saveCode = "Сохранить скрипт в файл...",
+    listOfFunctions = "Список используемых функций",
+    searchInputPlaceholder = "Введите название функции или метода",
+  }
+}
+-- // END OF LOCALISATION STRINGS //
+
+-- localisation helper function
+local M = {}
+local current_loc_data = loc_data["en"] -- default language: english
+
+-- set localisation language
+function M.load(lang)
+  current_loc_data = loc_data[lang] or loc_data["en"]
+end
+
+function M.get(key)
+  return current_loc_data[key] or loc_data["en"][key] or key
+end
+
+-- make table callable
+setmetatable(M, {
+  __call = function(_, key)
+    return M.get(key)
+  end
+})
+
+return M
--- _extensions/qcode/qcode.lua
+++ _extensions/qcode/qcode.lua
@@ -1,3 +1,7 @@
+local EXTENSION_NAME = "qcode"
+
+local l10n  = require("./localize")
+
 local function writeEnvironments()
   if quarto.doc.is_format("html:js") then
     quarto.doc.add_html_dependency({
@@ -69,28 +73,27 @@ local function addModal()
         x-transition:leave-start="zoom-leave-start"
         x-transition:leave-end="zoom-leave-end"
         x-cloak>
-          <div class="modal-card">
-            <div class="modal-toolbar">
-              <!-- <button @click="closeModal">Закрыть</button> -->
-              
-              <label title="Показать необязательные параметры">
-                <input type="checkbox" value="optParams" x-model="$store.vis.visFilter">
-                <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLXNldHRpbmdzMi1pY29uIGx1Y2lkZS1zZXR0aW5ncy0yIj48cGF0aCBkPSJNMTQgMTdINSIvPjxwYXRoIGQ9Ik0xOSA3aC05Ii8+PGNpcmNsZSBjeD0iMTciIGN5PSIxNyIgcj0iMyIvPjxjaXJjbGUgY3g9IjciIGN5PSI3IiByPSIzIi8+PC9zdmc+"/>
-                
-              </label>
+          <div class="modal-toolbar">
+            <div class="modal-toolbar-options">
+            <label title="]]..l10n("showOptionalParams")..[[">
+              <input type="checkbox" value="optParams" x-model="$store.vis.visFilter">
+              <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLXNldHRpbmdzMi1pY29uIGx1Y2lkZS1zZXR0aW5ncy0yIj48cGF0aCBkPSJNMTQgMTdINSIvPjxwYXRoIGQ9Ik0xOSA3aC05Ii8+PGNpcmNsZSBjeD0iMTciIGN5PSIxNyIgcj0iMyIvPjxjaXJjbGUgY3g9IjciIGN5PSI3IiByPSIzIi8+PC9zdmc+"/>
+            </label>
 
-              
-              <label title="Показать типы">
-                <input type="checkbox" value="typeHints" x-model="$store.vis.visFilter">
-                <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLXNoYXBlcy1pY29uIGx1Y2lkZS1zaGFwZXMiPjxwYXRoIGQ9Ik04LjMgMTBhLjcuNyAwIDAgMS0uNjI2LTEuMDc5TDExLjQgM2EuNy43IDAgMCAxIDEuMTk4LS4wNDNMMTYuMyA4LjlhLjcuNyAwIDAgMS0uNTcyIDEuMVoiLz48cmVjdCB4PSIzIiB5PSIxNCIgd2lkdGg9IjciIGhlaWdodD0iNyIgcng9IjEiLz48Y2lyY2xlIGN4PSIxNy41IiBjeT0iMTcuNSIgcj0iMy41Ii8+PC9zdmc+"/>
-              </label>
+            <label title="]]..l10n("showTypes")..[[">
+              <input type="checkbox" value="typeHints" x-model="$store.vis.visFilter">
+              <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLXNoYXBlcy1pY29uIGx1Y2lkZS1zaGFwZXMiPjxwYXRoIGQ9Ik04LjMgMTBhLjcuNyAwIDAgMS0uNjI2LTEuMDc5TDExLjQgM2EuNy43IDAgMCAxIDEuMTk4LS4wNDNMMTYuMyA4LjlhLjcuNyAwIDAgMS0uNTcyIDEuMVoiLz48cmVjdCB4PSIzIiB5PSIxNCIgd2lkdGg9IjciIGhlaWdodD0iNyIgcng9IjEiLz48Y2lyY2xlIGN4PSIxNy41IiBjeT0iMTcuNSIgcj0iMy41Ii8+PC9zdmc+"/>
+            </label>
 
-              
-              <label title="Показать диаграмму">
-                <input type="checkbox" value="blackBox" x-model="$store.vis.visFilter">
-                <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLW5ldHdvcmstaWNvbiBsdWNpZGUtbmV0d29yayI+PHJlY3QgeD0iMTYiIHk9IjE2IiB3aWR0aD0iNiIgaGVpZ2h0PSI2IiByeD0iMSIvPjxyZWN0IHg9IjIiIHk9IjE2IiB3aWR0aD0iNiIgaGVpZ2h0PSI2IiByeD0iMSIvPjxyZWN0IHg9IjkiIHk9IjIiIHdpZHRoPSI2IiBoZWlnaHQ9IjYiIHJ4PSIxIi8+PHBhdGggZD0iTTUgMTZ2LTNhMSAxIDAgMCAxIDEtMWgxMmExIDEgMCAwIDEgMSAxdjMiLz48cGF0aCBkPSJNMTIgMTJWOCIvPjwvc3ZnPg=="/>
-              </label>
+            <label title="]]..l10n("showDiagram")..[[">
+              <input type="checkbox" value="blackBox" x-model="$store.vis.visFilter">
+              <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLW5ldHdvcmstaWNvbiBsdWNpZGUtbmV0d29yayI+PHJlY3QgeD0iMTYiIHk9IjE2IiB3aWR0aD0iNiIgaGVpZ2h0PSI2IiByeD0iMSIvPjxyZWN0IHg9IjIiIHk9IjE2IiB3aWR0aD0iNiIgaGVpZ2h0PSI2IiByeD0iMSIvPjxyZWN0IHg9IjkiIHk9IjIiIHdpZHRoPSI2IiBoZWlnaHQ9IjYiIHJ4PSIxIi8+PHBhdGggZD0iTTUgMTZ2LTNhMSAxIDAgMCAxIDEtMWgxMmExIDEgMCAwIDEgMSAxdjMiLz48cGF0aCBkPSJNMTIgMTJWOCIvPjwvc3ZnPg=="/>
+            </label>
             </div>
+            <button class="btn-close" @click="closeModal">&times;</button>
+          </div>
+          <div class="modal-card">
+            
           <template x-if="$store.vis.selectedItem">
             <div style="overflow-y: auto;">
               <h4 class="signature">
@@ -122,7 +125,7 @@ local function addModal()
               
               <template x-if="parametersFilter($store.vis.selectedItem).length !== 0">
               <div>
-                <h4>Параметры</h4>
+                <h4>]]..l10n("parameters")..[[</h4>
                 <ul>
                   <template x-for="(param, index) in parametersFilter($store.vis.selectedItem)" :key="index">
                     <li>
@@ -135,7 +138,7 @@ local function addModal()
               </template>
               
                 <div>
-                  <h4>Возвращаемое значение</h4>
+                  <h4>]]..l10n("returnValue")..[[</h4>
 
                   <code 
                     x-show="$store.vis.visFilter.includes('typeHints')" 
@@ -144,15 +147,15 @@ local function addModal()
                   <span x-text="$store.vis.selectedItem.returns.description"></span>
                 </div>
 
-              <h4>Примеры</h4>
+              <h4>]]..l10n("examples")..[[</h4>
               <template x-for="(example, index) in $store.vis.selectedItem.examples" :key="index">
                 <div class="api__example">
                   <div class="api__example__input">
-                    <span class="vertical-text">Код</span>
+                    <span class="vertical-text">]]..l10n("codeExample")..[[</span>
                     <pre><code class="language-python" x-text="example.code"></code></pre>
                   </div>
                   <div class="api__example__output" x-show="example.output !== ''">
-                    <span class="vertical-text">Вывод</span>
+                    <span class="vertical-text">]]..l10n("codeOutput")..[[</span>
                     <code class="language-markdown" x-text="example.output"></code>
                   </div>
                   
@@ -160,7 +163,7 @@ local function addModal()
               </template>
 
               <p>
-                <a :href="$store.vis.selectedItem.url" target="_blank">Ссылка на документацию (англ.)</a>
+                <a :href="$store.vis.selectedItem.url" target="_blank">]]..l10n("linkToDocs")..[[</a>
               </p>
 
           </div>
@@ -184,37 +187,37 @@ function createSkulptIDE(block)
       <button class="sk__dropdown-button run" type="button"
         :disabled="isRunning"
         @click="run()"
-        title="Запустить">
+        title="]]..l10n("runCode")..[[">
         <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9IiNmZmZmZmYiIHN0cm9rZS13aWR0aD0iMiIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiBjbGFzcz0ibHVjaWRlIGx1Y2lkZS1wbGF5LWljb24gbHVjaWRlLXBsYXkiPjxwYXRoIGQ9Ik01IDVhMiAyIDAgMCAxIDMuMDA4LTEuNzI4bDExLjk5NyA2Ljk5OGEyIDIgMCAwIDEgLjAwMyAzLjQ1OGwtMTIgN0EyIDIgMCAwIDEgNSAxOXoiLz48L3N2Zz4=" />
       </button>
 
       <button class="sk__dropdown-button general" type="button"
-        title="Остановить скрипт"
+        title="]]..l10n("stopCode")..[["
         :disabled="!isRunning"
         @click="stop()">
         <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9IiNlNzY1ODUiIHN0cm9rZS13aWR0aD0iMiIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiBjbGFzcz0ibHVjaWRlIGx1Y2lkZS1zcXVhcmUtaWNvbiBsdWNpZGUtc3F1YXJlIj48cmVjdCB3aWR0aD0iMTgiIGhlaWdodD0iMTgiIHg9IjMiIHk9IjMiIHJ4PSIyIi8+PC9zdmc+" />
       </button>
 
       <button class="sk__dropdown-button general" type="button"
-        title="Скрыть результат работы скрипта"
+        title="]]..l10n("hideExecResults")..[["
         @click="clearOutput()">
         <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLXBhbmVsLWJvdHRvbS1jbG9zZS1pY29uIGx1Y2lkZS1wYW5lbC1ib3R0b20tY2xvc2UiPjxyZWN0IHdpZHRoPSIxOCIgaGVpZ2h0PSIxOCIgeD0iMyIgeT0iMyIgcng9IjIiLz48cGF0aCBkPSJNMyAxNWgxOCIvPjxwYXRoIGQ9Im0xNSA4LTMgMy0zLTMiLz48L3N2Zz4=" />
       </button>
 
       <button class="sk__dropdown-button general" type="button"
-        title="Восстановить исходный код"
+        title="]]..l10n("restoreSourceCode")..[["
         @click="resetCode()">
         <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLXJvdGF0ZS1jY3ctaWNvbiBsdWNpZGUtcm90YXRlLWNjdyI+PHBhdGggZD0iTTMgMTJhOSA5IDAgMSAwIDktOSA5Ljc1IDkuNzUgMCAwIDAtNi43NCAyLjc0TDMgOCIvPjxwYXRoIGQ9Ik0zIDN2NWg1Ii8+PC9zdmc+" />
       </button>
 
       <button class="sk__dropdown-button general" type="button"
-        title="Скопировать код"
+        title="]]..l10n("copyCode")..[["
         @click="copyCode()">
         <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLWZpbGVzLWljb24gbHVjaWRlLWZpbGVzIj48cGF0aCBkPSJNMTUgMmgtNGEyIDIgMCAwIDAtMiAydjExYTIgMiAwIDAgMCAyIDJoOGEyIDIgMCAwIDAgMi0yVjgiLz48cGF0aCBkPSJNMTYuNzA2IDIuNzA2QTIuNCAyLjQgMCAwIDAgMTUgMnY1YTEgMSAwIDAgMCAxIDFoNWEyLjQgMi40IDAgMCAwLS43MDYtMS43MDZ6Ii8+PHBhdGggZD0iTTUgN2EyIDIgMCAwIDAtMiAydjExYTIgMiAwIDAgMCAyIDJoOGEyIDIgMCAwIDAgMS43MzItMSIvPjwvc3ZnPg==" />
       </button>
 
       <button class="sk__dropdown-button general" type="button"
-        title="Сохранить скрипт в файл"
+        title="]]..l10n("saveCode")..[["
         @click="saveToFile()">
         <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLXNhdmUtaWNvbiBsdWNpZGUtc2F2ZSI+PHBhdGggZD0iTTE1LjIgM2EyIDIgMCAwIDEgMS40LjZsMy44IDMuOGEyIDIgMCAwIDEgLjYgMS40VjE5YTIgMiAwIDAgMS0yIDJINWEyIDIgMCAwIDEtMi0yVjVhMiAyIDAgMCAxIDItMnoiLz48cGF0aCBkPSJNMTcgMjF2LTdhMSAxIDAgMCAwLTEtMUg4YTEgMSAwIDAgMC0xIDF2NyIvPjxwYXRoIGQ9Ik03IDN2NGExIDEgMCAwIDAgMSAxaDciLz48L3N2Zz4=" />
       </button>
@@ -230,7 +233,7 @@ function createSkulptIDE(block)
     </div>
 
     <div class="sk__editor__func" x-show="functionNames.length !== 0" x-transition>
-    <img title="Список используемых функций" style="height: 18px;" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9IiM1MTU3NmQiIHN0cm9rZS13aWR0aD0iMiIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiBjbGFzcz0ibHVjaWRlIGx1Y2lkZS1ib29rLW1hcmtlZC1pY29uIGx1Y2lkZS1ib29rLW1hcmtlZCI+PHBhdGggZD0iTTEwIDJ2OGwzLTMgMyAzVjIiLz48cGF0aCBkPSJNNCAxOS41di0xNUEyLjUgMi41IDAgMCAxIDYuNSAySDE5YTEgMSAwIDAgMSAxIDF2MThhMSAxIDAgMCAxLTEgMUg2LjVhMSAxIDAgMCAxIDAtNUgyMCIvPjwvc3ZnPg=="/>
+    <img title="]]..l10n("listOfFunctions")..[[" style="height: 18px;" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9IiM1MTU3NmQiIHN0cm9rZS13aWR0aD0iMiIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiBjbGFzcz0ibHVjaWRlIGx1Y2lkZS1ib29rLW1hcmtlZC1pY29uIGx1Y2lkZS1ib29rLW1hcmtlZCI+PHBhdGggZD0iTTEwIDJ2OGwzLTMgMyAzVjIiLz48cGF0aCBkPSJNNCAxOS41di0xNUEyLjUgMi41IDAgMCAxIDYuNSAySDE5YTEgMSAwIDAgMSAxIDF2MThhMSAxIDAgMCAxLTEgMUg2LjVhMSAxIDAgMCAxIDAtNUgyMCIvPjwvc3ZnPg=="/>
     <template x-for="(func, index) in functionNames" :key="index">
       <code x-text="func" @click="openModal(func)"></code>
     </template>
@@ -256,7 +259,7 @@ function createSkAPISearch(div)
       <div class="search__form">
         <input
           type="text"
-          placeholder="Введите название функции или метода"
+          placeholder="]]..l10n("searchInputPlaceholder")..[["
           @input.debounce="searchDef"
           x-model.fill="query"
         />
@@ -274,24 +277,45 @@ function createSkAPISearch(div)
   return pandoc.Div(content, { class = "sk-api-search__formated" })
 end
 
-if quarto.doc.isFormat("html:js") then
-  CodeBlock = function(block)
-    if block.classes:includes("sk-python") then
-      addAceLibsOnce()
-      writeEnvironments()
-      addModal()
-      return createSkulptIDE(block)
-    end
-    return nil
-  end
+local function render_elements(options)
 
-  Div = function(div)
-    if div.classes:includes("sk-api-search") then -- если div содержит нужный стиль - обрабатываем разметку
+  l10n.load(options.lang)
+
+  return {
+    CodeBlock = function(block)
+      if quarto.doc.isFormat("html:js") and block.classes:includes("sk-python") then
         addAceLibsOnce()
         writeEnvironments()
         addModal()
-        return createSkAPISearch(div)
+        return createSkulptIDE(block)
+      end
+      return nil
+    end,
+    Div = function(div)
+      if quarto.doc.isFormat("html:js") and div.classes:includes("sk-api-search") then -- если div содержит нужный стиль - обрабатываем разметку
+          addAceLibsOnce()
+          writeEnvironments()
+          addModal()
+          return createSkAPISearch(div)
+      end
+      return nil
     end
-    return nil
+  }
+end
+
+function Pandoc(doc)
+  -- default options
+  local options = {
+    lang = pandoc.utils.stringify(doc.meta.lang)
+  }
+
+  -- replace default option with local 
+  local globalOptions = doc.meta[EXTENSION_NAME]
+  if type(globalOptions) == "table" then
+    for k, v in pairs(globalOptions) do
+      options[k] = pandoc.utils.stringify(v)
+    end
   end
+
+  return doc:walk(render_elements(options))
 end
